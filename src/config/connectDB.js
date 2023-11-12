e3764121a968d3cdc47417910daf9e08535b3a10
doandc701 <88759481+doandc701@users.noreply.github.com>
2023-11-12 16:23:42 +0700

    up1

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,23 +1,7 @@
-import { MongoClient } from "mongodb";
 import mongoose from "mongoose";
 import "dotenv/config";
 
-let dbConnetion;
-const uriCloud =
-  "mongodb+srv://books:j7yjNzJntNrwXHxp@clbook.lvbipmz.mongodb.net/?retryWrites=true&w=majority";
-const uriLocal = "mongodb://localhost:27017/test";
-export const connectToDb = (callback) => {
-  MongoClient.connect(uriCloud)
-    .then((client) => {
-      dbConnetion = client.db();
-      return callback();
-    })
-    .catch((error) => {
-      console.log(error);
-      return callback(error);
-    });
-};
-export const getDb = () => dbConnetion;
+
 export const connectMongoose = async () => {
   try {
     await mongoose.connect(process.env.URL_CONNECT_MONGODB);
--- src/config/db.js
+++ src/config/db.js
@@ -1,22 +1,4 @@
-import { MongoClient } from "mongodb";
 import mongoose from "mongoose";
-
-let dbConnetion;
-const uriCloud =
-  "mongodb+srv://books:j7yjNzJntNrwXHxp@clbook.lvbipmz.mongodb.net/?retryWrites=true&w=majority";
-const uriLocal = "mongodb://localhost:27017/test";
-export const connectToDb = (callback) => {
-  MongoClient.connect(uriCloud)
-    .then((client) => {
-      dbConnetion = client.db();
-      return callback();
-    })
-    .catch((error) => {
-      console.log(error);
-      return callback(error);
-    });
-};
-export const getDb = () => dbConnetion;
 
 export const connectMongoose = async () => {
   try {
