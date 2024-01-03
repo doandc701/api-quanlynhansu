c89f3a3d08bf3e3155b1c5a41cf51ea8908424d8
doandc701 <88759481+doandc701@users.noreply.github.com>
2024-01-03 21:47:10 +0700

    wip

--- src/middlewares/rateLimit.js
+++ src/middlewares/rateLimit.js
@@ -1,19 +1,19 @@
-import ratelimit from "express-rate-limit";
+// import ratelimit from "express-rate-limit";
 
-export const apiLimit = ratelimit({
-  windowMs: 60 * 1000,
-  max: 10,
-  message: "Too many connection",
-  standardHeaders: true,
-  legacyHeaders: false,
-  handler: (req, res) => {
-    res.status(429).send({
-      status: 500,
-      message: "Too man",
-    });
-  },
-  skip: (req, res) => {
-    // if (req.ip === "::1") return true;
-    return false;
-  },
-});
+// export const apiLimit = ratelimit({
+//   windowMs: 60 * 1000,
+//   max: 10,
+//   message: "Too many connection",
+//   standardHeaders: true,
+//   legacyHeaders: false,
+//   handler: (req, res) => {
+//     res.status(429).send({
+//       status: 500,
+//       message: "Too man",
+//     });
+//   },
+//   skip: (req, res) => {
+//     // if (req.ip === "::1") return true;
+//     return false;
+//   },
+// });
