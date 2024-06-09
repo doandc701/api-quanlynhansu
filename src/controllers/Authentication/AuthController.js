3028cd4b019b57bd98d8bcb60706eb9355b0140c
Nguyen Xuan Doan <nguyenxuandoan701scsoft@gmail.com>
2024-06-09 15:13:08 +0700

    stop use redis for number input password

--- src/controllers/Authentication/AuthController.js
+++ src/controllers/Authentication/AuthController.js
@@ -2,17 +2,17 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import sanitize from "mongo-sanitize"; // ngan chan injection
 import ip from "ip";
-import { client } from "../../services/redis.service.js";
+// import { client } from "../../services/redis.service.js";
 import { ObjectDatabase } from "../../models/auth/index.js";
 import Department from "../../models/department.model.js";
 import Branchs from "../../models/branch.model.js";
 import Image from "../../models/image.model.js";
 import { getStorage, ref, deleteObject } from "firebase/storage";
 import { signInWithEmailAndPassword } from "@firebase/auth";
-import {
-  checkLoginAttempts,
-  setLoginAttempts,
-} from "../../middlewares/loginAccountLimiter.js";
+// import {
+//   checkLoginAttempts,
+//   setLoginAttempts,
+// } from "../../middlewares/loginAccountLimiter.js";
 import { verifyTokenUser } from "../../middlewares/authJwt.js";
 
 import analytics from "../../config/firebase.config.js";
@@ -38,13 +38,13 @@ const authFirebase = await signInWithEmailAndPassword(
   analytics,
   process.env.FIREBASE_USER,
   process.env.FIREBASE_AUTH
-).catch(() => {});
+).catch(() => { });
 const storageFB = getStorage();
 
 async function LIST_USER(req, res) {
   const newQuery = req.query;
   const searchCondition = {};
-  const countRecord = await USER.countDocuments().catch(() => {});
+  const countRecord = await USER.countDocuments().catch(() => { });
   if (newQuery) {
     const skip =
       Number(newQuery.page) * Number(newQuery.limit) - Number(newQuery.limit);
@@ -66,7 +66,7 @@ async function LIST_USER(req, res) {
     const results = await query
       .skip(skip)
       .limit(newQuery.limit)
-      .catch(() => {});
+      .catch(() => { });
 
     return res.status(200).json({
       data: results,
@@ -79,7 +79,7 @@ async function LIST_USER(req, res) {
 
 async function GET_USER(req, res, next) {
   const recordUsers = await USER.findOne({ code: req.params.code }).catch(
-    () => {}
+    () => { }
   );
   res.status(200).json({
     data: recordUsers,
@@ -151,7 +151,7 @@ async function PUT_USER(req, res) {
 
 async function DELETE_USER(req, res) {
   const recorDeleteUser = await USER.findOne({ code: req.params.code }).catch(
-    () => {}
+    () => { }
   );
   if (!recorDeleteUser) {
     return res.status(404).json({ message: "Không tìm thấy user cần xóa!" });
@@ -160,7 +160,7 @@ async function DELETE_USER(req, res) {
   if (recorDeleteUser.avatar_id) {
     const recorDeleteImg = await Image.findOne({
       _id: recorDeleteUser.avatar_id,
-    }).catch(() => {});
+    }).catch(() => { });
     if (!recorDeleteImg) {
       return res.status(404).json({ message: "Không tìm thấy ảnh cần xóa!" });
     }
@@ -172,8 +172,8 @@ async function DELETE_USER(req, res) {
     }
     const fileName = `images/${recorDeleteImg.origin_name}`;
     const fileRef = ref(storageFB, fileName);
-    await deleteObject(fileRef).catch(() => {});
-    await Image.deleteOne({ _id: recorDeleteImg._id }).catch(() => {});
+    await deleteObject(fileRef).catch(() => { });
+    await Image.deleteOne({ _id: recorDeleteImg._id }).catch(() => { });
     USER.deleteOne({ code: recorDeleteUser.code })
       .then((deleted) => {
         res.status(200).json(deleted);
@@ -195,7 +195,7 @@ async function DELETE_USER(req, res) {
 const SignUp = async (req, res) => {
   const checkMailExits = await USER.findOne({
     email: sanitize(req.body.email),
-  }).catch(() => {});
+  }).catch(() => { });
   if (checkMailExits) {
     res.status(422).json({ message: "Lỗi! Email này đã tồn tại." });
     return;
@@ -255,74 +255,117 @@ const SignUp = async (req, res) => {
 };
 
 const SignIn = async (req, res, next) => {
-  let checkRedis = await checkLoginAttempts(ip.address());
-  if (checkRedis?.pass) {
-    // USER.findOne({
-    //   $or: [
-    //     { username: sanitize(req.body.username) },
-    //     { email: sanitize(req.body.email) },
-    //   ],
-    // })
-    USER.findOne({
-      email: sanitize(req.body.email),
-    })
-      .populate("role_id", "-__v")
-      .then(async (user) => {
-        if (!user) {
-          res
-            .status(400)
-            .json({ message: "Tài khoản hoặc mật khẩu không đúng !" });
-          return;
-        }
-        const passwordIsValid = bcrypt.compareSync(
-          req.body.password,
-          user.password
-        );
-        if (!passwordIsValid) {
-          checkRedis = await setLoginAttempts(ip.address());
-          const remaining = 3 - parseInt(checkRedis.data.count);
-          res.status(404).json({
-            message: `${
-              remaining
-                ? `Bạn còn ${remaining} lần nhập`
-                : "Tài khoản hoặc mật khẩu không đúng !"
-            }`,
-          });
-          return;
-        }
+  // let checkRedis = await checkLoginAttempts(ip.address());
+  // if (checkRedis?.pass) {
+  //   // USER.findOne({
+  //   //   $or: [
+  //   //     { username: sanitize(req.body.username) },
+  //   //     { email: sanitize(req.body.email) },
+  //   //   ],
+  //   // })
+  //   USER.findOne({
+  //     email: sanitize(req.body.email),
+  //   })
+  //     .populate("role_id", "-__v")
+  //     .then(async (user) => {
+  //       if (!user) {
+  //         res
+  //           .status(400)
+  //           .json({ message: "Tài khoản hoặc mật khẩu không đúng !" });
+  //         return;
+  //       }
+  //       const passwordIsValid = bcrypt.compareSync(
+  //         req.body.password,
+  //         user.password
+  //       );
+  //       if (!passwordIsValid) {
+  //         checkRedis = await setLoginAttempts(ip.address());
+  //         const remaining = 3 - parseInt(checkRedis.data.count);
+  //         res.status(404).json({
+  //           message: `${
+  //             remaining
+  //               ? `Bạn còn ${remaining} lần nhập`
+  //               : "Tài khoản hoặc mật khẩu không đúng !"
+  //           }`,
+  //         });
+  //         return;
+  //       }
 
-        const token = jwt.sign({ id: user.id }, process.env.TOKEN_SECRET, {
-          expiresIn: 86400, // 24 hours
-        });
-        // let authorities = [];
-        // for (let i = 0; i < user.role_id.length; i++) {
-        //   authorities.push(user.role_id[i].name.toUpperCase());
-        // }
-        res.status(200).json({
-          data: {
-            data: user,
-            accessToken: token,
-          },
-          message: "Đăng nhập thành công.",
-        });
-      })
-      .catch(() => {});
-  }
-  if (!checkRedis?.pass) {
-    if (checkRedis?.data) {
-      const wait1 = checkRedis.wait;
-      const remaining = 3 - parseInt(checkRedis.data.count);
-      if (remaining <= 0) {
-        return res.status(404).json({
-          message: `Tài khoản của bạn đã bị khóa, vui lòng thử lại sau ${wait1} giây.`,
-        });
-      } else {
-        return res.status(404).json({
-          message: `Bạn còn ${remaining} lần nhập`,
-        });
+  //       const token = jwt.sign({ id: user.id }, process.env.TOKEN_SECRET, {
+  //         expiresIn: 86400, // 24 hours
+  //       });
+  //       // let authorities = [];
+  //       // for (let i = 0; i < user.role_id.length; i++) {
+  //       //   authorities.push(user.role_id[i].name.toUpperCase());
+  //       // }
+  //       res.status(200).json({
+  //         data: {
+  //           data: user,
+  //           accessToken: token,
+  //         },
+  //         message: "Đăng nhập thành công.",
+  //       });
+  //     })
+  //     .catch(() => {});
+  // }
+  // if (!checkRedis?.pass) {
+  //   if (checkRedis?.data) {
+  //     const wait1 = checkRedis.wait;
+  //     const remaining = 3 - parseInt(checkRedis.data.count);
+  //     if (remaining <= 0) {
+  //       return res.status(404).json({
+  //         message: `Tài khoản của bạn đã bị khóa, vui lòng thử lại sau ${wait1} giây.`,
+  //       });
+  //     } else {
+  //       return res.status(404).json({
+  //         message: `Bạn còn ${remaining} lần nhập`,
+  //       });
+  //     }
+  //   }
+  // }
+  USER.findOne({
+    email: sanitize(req.body.email),
+  })
+    .populate("role_id", "-__v")
+    .then(async (user) => {
+      if (!user) {
+        res
+          .status(400)
+          .json({ message: "Tài khoản hoặc mật khẩu không đúng !" });
+        return;
       }
-    }
-  }
+      const passwordIsValid = bcrypt.compareSync(
+        req.body.password,
+        user.password
+      );
+      // if (!passwordIsValid) {
+      //   checkRedis = await setLoginAttempts(ip.address());
+      //   const remaining = 3 - parseInt(checkRedis.data.count);
+      //   res.status(404).json({
+      //     message: `${remaining
+      //       ? `Bạn còn ${remaining} lần nhập`
+      //       : "Tài khoản hoặc mật khẩu không đúng !"
+      //       }`,
+      //   });
+      //   return;
+      // }
+
+      const token = jwt.sign({ id: user.id }, process.env.TOKEN_SECRET, {
+        expiresIn: 86400, // 24 hours
+      });
+      // let authorities = [];
+      // for (let i = 0; i < user.role_id.length; i++) {
+      //   authorities.push(user.role_id[i].name.toUpperCase());
+      // }
+      res.status(200).json({
+        data: {
+          data: user,
+          accessToken: token,
+        },
+        message: "Đăng nhập thành công.",
+      });
+    })
+    .catch(() => { });
 };
 
 async function ChangePass(req, res) {
